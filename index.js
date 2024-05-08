3c2da5274eca2c1335d353817378cf6e6c6fbba7
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-08 16:07:04 +0300

    Add logic to play single round
    I decided to put logic for determining the winner
    in the separate function to make code more readable
    and maintainable.

--- index.js
+++ index.js
@@ -23,9 +23,52 @@ function getComputerChoice() {
     return shapes[Math.floor(Math.random() * 3)];
 }
 
+function playRound(humanChoice, computerChoice) {
+    const roundResult = determineRoundResult(humanChoice, computerChoice);
+    switch(roundResult) {
+        case 'c': 
+            console.log("You lost, seems like robots are gonna replace us soon D:");
+            computerScore += 1;
+            break;
+        case 'h':
+            console.log("Wow, human won this round!");
+            humanScore += 1;
+            break;
+        default:
+            console.log("That's a tie!\nLet's play one more time!");
+            break;
+    }
+}
 
-// userShape = getHumanChoice();
-// console.log(userShape);
+function determineRoundResult(humanChoice, computerChoice) {
+    if (humanChoice[0] === computerChoice[0]) return;
 
-computerShape = getComputerChoice();
-console.log(computerShape);
+    switch(humanChoice[0]) {
+        case 'r':
+            switch(computerChoice[0]) {
+                case 'p': return 'c';
+                case 's': return 'h';
+            }
+        case 'p':
+            switch(computerChoice[0]) {
+                case 'r': return 'h';
+                case 's': return 'c';
+            }
+        case 's':
+            switch(computerChoice[0]) {
+                case 'r': return 'c';
+                case 'p': return 'h';
+            }
+    }
+}
+
+
+humanChoice = getHumanChoice();
+console.log(humanChoice);
+
+computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+let humanScore = 0
+let computerScore = 0;
+playRound(humanChoice, computerChoice);
