d44257ffed7435fc79e3620918a72c1df305af6b
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-08 15:10:59 +0300

    Implement user input with validation
    The program keeps asking a player to enter a shape
    until he write the correct option.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello Wolk!");
+function getHumanChoice() {
+    let userShape = null;
+    while (!validateUserInput(userShape)) {
+        userShape = prompt("Please, enter your shape R - rock, P - paper or S - scissors");
+    }
+    return userShape.toLocaleLowerCase();
+}
+
+function validateUserInput(shape) {
+    if (!shape || shape.length === 0) return false;
+    switch(shape[0].toLocaleLowerCase()) {
+        case 'r':
+        case 'p':
+        case 's':
+            return true;
+    }
+
+    return false;
+}
+
+
+userShape = getHumanChoice();
+console.log(userShape);
