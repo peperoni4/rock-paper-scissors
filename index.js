e069a679d6830af763ac11d54763223d8f49401b
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-11 20:44:59 +0300

    Add game reset and modify some styles

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const shapeButtons = document.querySelector(".container").children;
 const roundResultText = document.querySelector(".roundResult");
 const gameResultText = document.querySelector(".gameResult");
 const scoreText = document.querySelector(".score");
+const combinationInfo = document.querySelector(".combination-info");
+const infoText = document.querySelector(".infoText");
 
 const pointsToWin = 3;
 const gameScore = { humanScore: 0, computerScore: 0 };
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    resetGame();
+  }
+});
 
 for (const btn of shapeButtons) {
   btn.addEventListener("click", onShapeButtonClick);
@@ -23,6 +31,23 @@ function onShapeButtonClick(e) {
   ) {
     const winner = gameScore.computerScore === pointsToWin ? "🤖" : "🦧";
     gameResultText.textContent = `${winner} won the game!`;
+    gameResultText.style.color = "gold";
+    infoText.style.display = "block";
+    for (const btn of shapeButtons) {
+      btn.removeEventListener("click", onShapeButtonClick);
+    }
+  }
+}
+
+function resetGame() {
+  gameScore.computerScore = gameScore.humanScore = 0;
+  scoreText.textContent = "Rock Paper Scissors";
+  roundResultText.textContent = "Click on the shape to start the game";
+  combinationInfo.textContent = "";
+  gameResultText.textContent = "";
+  infoText.style.display = "none";
+  for (const btn of shapeButtons) {
+    btn.addEventListener("click", onShapeButtonClick);
   }
 }
 
@@ -39,18 +64,29 @@ function playRound(humanChoice, computerChoice) {
   const roundResult = determineRoundResult(humanChoice, computerChoice);
   switch (roundResult) {
     case "computer":
-      roundResultText.textContent =
-        `You lost, seems like robots are gonna replace us soon D:` +
-        `\n${capitalize(computerChoice)} beats ${humanChoice}.`;
+      roundResultText.textContent = `You lost, seems like robots are gonna replace us soon D:`;
+      combinationInfo.textContent = `${capitalize(
+        computerChoice
+      )} beats ${humanChoice}.`;
+
+      combinationInfo.style.color = "rgb(235, 47, 47)";
       gameScore.computerScore += 1;
       break;
     case "human":
-      roundResultText.textContent =
-        `Wow, human won this round!` +
-        `\n${capitalize(humanChoice)} beats ${computerChoice}.`;
+      roundResultText.textContent = `Wow, human won this round!`;
+      combinationInfo.textContent = `${capitalize(
+        humanChoice
+      )} beats ${computerChoice}.`;
+
+      combinationInfo.style.color = "green";
       gameScore.humanScore += 1;
       break;
     default:
+      combinationInfo.textContent`${capitalize(
+        computerChoice
+      )} and ${humanChoice} :O`;
+
+      combinationInfo.style.color = "grey";
       roundResultText.textContent = "That's a tie!\nLet's play one more time!";
       break;
   }
